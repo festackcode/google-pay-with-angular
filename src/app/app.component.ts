14eb4cfff2e43e4e93705cda45ed6e3ff18089d2
festackcode <tecstack.io@gmail.com>
2020-11-27 21:23:18 +0100

    Google pay in Angular Demo App

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Google Pay Demo';
+
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['AMEX', 'VISA', 'MASTERCARD']
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '0.10',
+      currencyCode: 'EUR',
+      countryCode: 'BE'
+    },
+    callbackIntents: ['PAYMENT_AUTHORIZATION']
+  };
+
+  onLoadPaymentData = (
+    event: Event
+  ): void => {
+    const eventDetail = event as CustomEvent<google.payments.api.PaymentData>;
+    console.log('load payment data', eventDetail.detail);
+  }
+
+  onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = (
+    paymentData
+    ) => {
+      console.log('payment authorized', paymentData);
+      return {
+        transactionState: 'SUCCESS'
+      };
+    }
+
+  onError = (event: ErrorEvent): void => {
+    console.error('error', event.error);
+  }
+}
